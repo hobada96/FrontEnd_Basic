25648b0dc5b9252ac64fa19cb612c1a24f8ea4e2
hobada96 <hobada96@gmail.com>
2020-12-09 17:47:13 +0900

    Add list example

--- Blog/front-end/mobile/App.tsx
+++ Blog/front-end/mobile/App.tsx
@@ -1,12 +1,30 @@
 import React, {useState} from 'react';
-import {Button, Text} from 'react-native';
+import {Button, FlatList, Text, TextInput} from 'react-native';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [data, onChangeText] = useState('Hello');
   return (
     <>
       <Text> Count: {count}</Text>
       <Button title={'click'} onPress={() => setCount(count + 1)} />
+      <TextInput onChangeText={(str) => onChangeText(str)} value={data} />
+      <Text>{data}</Text>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
     </>
   );
 };
