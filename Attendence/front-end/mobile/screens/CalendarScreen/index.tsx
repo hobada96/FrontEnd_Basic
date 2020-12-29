cdad2d4b10a610e3dcd5fdfa216f1eac2e02557f
hobada96 <hobada96@gmail.com>
2020-12-29 23:06:22 +0900

    setting commit

--- Attendence/front-end/mobile/screens/CalendarScreen/index.tsx
+++ Attendence/front-end/mobile/screens/CalendarScreen/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+
+const koMonthName = [
+  '1월',
+  '2월',
+  '3월',
+  '4월',
+  '5월',
+  '6월',
+  '7월',
+  '8월',
+  '9월',
+  '10월',
+  '12월',
+];
+const koDayNames = ['월', '화', '수', '목', '금', '토', '일'];
+// const attendanceDate = []; 출석한날을 Calendar에 표시, 어떻게 값을 넘기고 받을것인가 고민할 것.
+const setCalendarLocales = (locale: string) => {
+  LocaleConfig.locales[locale] = {
+    monthNames: koMonthName,
+    monthNamesShort: koMonthName,
+    dayNames: koMonthName,
+    dayNamesShort: koDayNames,
+  };
+};
 
 export default () => {
+  const locale = 'ko';
+  setCalendarLocales(locale);
+  LocaleConfig.defaultLocale = locale;
   return (
     <>
-      <Text>Calendar</Text>
+      <Calendar />
     </>
   );
 };
--- Attendence/front-end/mobile/screens/router.tsx
+++ Attendence/front-end/mobile/screens/router.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'; // import react를 해주지 않으면 에러가 생김.
-import {HomeScreen, CalendarScreen, LoginScreen} from './index';
+import {HomeScreen, CalendarScreen, LoginScreen, TestScreen} from './index';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -12,12 +12,12 @@ const screen = () => {
     <>
       <Tab.Navigator>
         <Tab.Screen name="QR코드" component={HomeScreen} />
-        <Tab.Screen name="내 정보" component={CalendarScreen} />
+        <Tab.Screen name="캘린더" component={CalendarScreen} />
+        <Tab.Screen name="Test" component={TestScreen} />
       </Tab.Navigator>
     </>
   );
 };
-
 export default () => {
   return (
     <>
