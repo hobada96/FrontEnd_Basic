fe24a132a189c1a8f060bd748162944cc9f86d6a
hobada96 <hobada96@gmail.com>
2021-03-20 23:27:10 +0900

    todo example

--- react/redux-ex5/redux/src/App.js
+++ react/redux-ex5/redux/src/App.js
@@ -1,8 +1,17 @@
+import {useDispatch, useSelector} from 'react-redux'
+import {getText} from "./redux/action";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const state = useSelector(state => state);
+    const dispatch = useDispatch();
+    const {text} = state;
+    console.log(state);
+    return (
+        <div className="App">
+            <h1>{text}</h1>
+            <input onChange={(e) => dispatch(getText(e.target.value))} />
+        </div>
+    );
 }
 
 export default App;
